d1689b573a4c365aac8af9db3b7f66fa3c737d30
microegor <microegor321@gmail.com>
2026-08-05 14:17:40 +0200

    feat: add Preloader component with styles and stories

---
+++ src/components/Loader/Preloader.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Preloader } from "./Preloader";
+
+const meta = {
+  title: "Components/Preloader",
+  component: Preloader,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Preloader>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/Loader/Preloader.tsx
@@ -0,0 +1,5 @@
+import styles from "./Preloader.module.css";
+
+export function Preloader({}) {
+  return <div className={styles.loader} aria-hidden="true" />;
+}
