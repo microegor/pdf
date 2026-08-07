dd77d8295ef2d5ca2f5389eabc439714f611e5fd
microegor <microegor321@gmail.com>
2026-08-07 10:47:14 +0200

    feat: implement ToggleButton component with styling and structure

--- src/components/Togglebutton/TButton.tsx
+++ src/components/Togglebutton/TButton.tsx
@@ -1,9 +1,11 @@
 import type { PropsWithChildren } from "react";
 
+import styles from "./ToggleButton.module.css";
+
 type TButtonProps = PropsWithChildren<{
     onClick?: () => void;
 }>;
 
 export function TButton({ children}: TButtonProps) {
-    <button>{children}</button>
+    return(<button className={styles.tButton}>{children}</button>)
 }
--- src/components/Togglebutton/Togglebutton.tsx
+++ src/components/Togglebutton/Togglebutton.tsx
@@ -1,17 +1,30 @@
 import { Children, type PropsWithChildren } from "react";
 
-import styles from ToggleButton.module.css;
+import styles from "ToggleButton.module.css";
 
 type ToggleButtonProps = PropsWithChildren;
 
 export function ToggleButton({ children }: ToggleButtonProps) {
     const buttons = Children.toArray(children);
 
+
     return (
-        <div>
-            {buttons.map((button, index) => (
-                <div key={index}>{button}</div>
-            ))}
+        <div className={styles.toggleButton}>
+            {buttons.map((button, index) => {
+                const isFirst = index === 0;
+                const isLast = index === buttons.length - 1;
+
+                return (
+                    <div
+                        key={index}
+                        className={
+                            isFirst ? styles.first : isLast ? styles.last : styles.middle
+                        }
+                    >
+                        {button}
+                    </div>
+                );
+            })}
         </div>
     );
 }
