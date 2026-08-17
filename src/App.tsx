8c0ef7021baff6246fcde8fb1bd502bea4249c5f
microegor <microegor321@gmail.com>
2026-08-17 18:28:00 +0200

    feat: server deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import { useState } from "react";
 import "../style/App.css";
 import { Button } from "./components/Button";
 import { Stack } from "./components/Stack";
-import { Switch } from "./components/Switch";
-import { Tabs } from "./components/Tabs";
-import { Tab } from "./components/Tabs";
 import { Preloader } from "./components/Loader";
-import { Accordion } from "./components/Accordion";
-import { AccordionItem } from "./components/Accordion";
-import { ToggleButton } from "./components/ToggleButton";
-import { ToggleButtonGroup } from "./components/ToggleButton";
-import { TreeContainer } from "./components/Tree";
-import { TreeNode } from "./components/Tree";
 import { Modal } from "./components/Modal";
 
 function App() {
@@ -29,40 +20,9 @@ function App() {
     }
 
     setPdfFile(file);
-  };
 
-  const handleSavePdf = async () => {
-    if (!pdfFile) {
-      alert("Сначала выберите PDF файл");
-      return;
-    }
-
-    const formData = new FormData();
-
-    formData.append("pdf", pdfFile);
-
-    try {
-      const response = await fetch("http://localhost:3001/upload", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error("Ошибка загрузки");
-      }
-
-      const data = await response.json();
-
-      console.log(data);
-
-      alert("PDF сохранён");
-
-      setPdfFile(null);
-      handleCloseModal();
-    } catch (error) {
-      console.error(error);
-      alert("Не удалось сохранить PDF");
-    }
+    // Здесь уже доступен сам файл
+    console.log("Полученный PDF:", file);
   };
 
   const handleOpenModal = () => {
@@ -88,15 +48,17 @@ function App() {
         sx={{
           width: "100%",
           height: 70,
-
           alignItems: "center",
           justifyContent: "center",
-
           border: "1px solid #ccc",
         }}
       >
-        <Button size="big" variant="contained" text="Add" onClick={handleOpenModal}>
-        </Button>
+        <Button
+          size="big"
+          variant="contained"
+          text="Add"
+          onClick={handleOpenModal}
+        />
 
         <Modal
           open={modalOpen}
@@ -115,14 +77,8 @@ function App() {
               Выбран файл: {pdfFile.name}
             </p>
           )}
-
-          <Button
-            size="big"
-            variant="contained"
-            text="Сохранить"
-            onClick={handleSavePdf}
-          />
         </Modal>
+
         <div>
           <Preloader />
         </div>
@@ -137,51 +93,15 @@ function App() {
           minHeight: 0,
         }}
       >
-        <Stack direction="column" spacing={1} sx={{ width: 300, height: "100%", overflow: "auto" }}>
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
-          <Preloader />
+        <Stack
+          direction="column"
+          spacing={1}
+          sx={{
+            width: 300,
+            height: "100%",
+            overflow: "auto",
+          }}
+        >
           <Preloader />
           <Preloader />
           <Preloader />
